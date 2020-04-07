23dc2ea7ed252288b1758038f4a5a14d54d34d44
Eric Maxey <eric.maxey@gmail.com>
2020-04-06 17:32:02 -0700

    ok

--- main.js
+++ main.js
@@ -1,13 +1,32 @@
 var myItems = {
-    availableFloorSpace: 1,
-    availableCargoSpace: 100,
+    floorSpace: 2,
+    availableFloorSpace: 2,
     availableEnergyStorage: 100,
 
-    // energy: 0,
-    // solarPanels: 2,
-    // nutrientStations: 2,
-    // crewMembers: 2,
+    // // ironOre: 99,
+    // energy: 100,
+
+    // // energy: 0,
+    // // solarPanels: 2,
+    // // nutrientStations: 2,
+    // // crewMembers: 2,
+    // // silicon: 100,
+    // // iron: 100,
+    // siliconOre: 10,
+    // ironOre: 10,
 }
+
+destroyableItems = [
+    "cargoContainers",
+    "ironOre",
+    "siliconOre",
+    "iron",
+    "silicon",
+    "solarPanels",
+    "crewMembers",
+    "deadBodies",
+    "nutrientStations",
+];
 
 function haveItems() {
     for (item in this.args) {
@@ -37,7 +56,7 @@ var actions = [
     },
     {
         name: "MineAsteroid",
-        tooltip: "Cost: 4 energy",
+        tooltip: "Cost: 2 energy\n Effects: 1 iron ore or 1 silicon ore",
         function: search,
         args: {
             ironOre: 0.5,
@@ -54,7 +73,7 @@ var actions = [
     },
     {
         name: "SmeltIron",
-        tooltip: "Cost: 2 iron ore, 1 energy",
+        tooltip: "Cost: 2 iron ore, 1 energy\nEffects: 1 iron",
         function: craft,
         args: {
             ironOre: -2,
@@ -67,7 +86,7 @@ var actions = [
     },
     {
         name: "SmeltSilicon",
-        tooltip: "Cost: 2 silicon ore, 1 energy",
+        tooltip: "Cost: 2 silicon ore, 1 energy\nEffects: 1 silicon",
         function: craft,
         args: {
             siliconOre: -2,
@@ -80,11 +99,12 @@ var actions = [
     },
     {
         name: "BuildSolarPanel",
-        tooltip: "Cost: 1 silicon, 1 iron",
+        tooltip: "Cost: 1 silicon, 1 iron, 1 floor space\nEffects: 1 energy per second",
         function: craft,
         args: {
             silicon: -1,
             iron: -1,
+            availableFloorSpace: -1,
             solarPanels: 1,
         },
         duration: 8,
@@ -93,7 +113,7 @@ var actions = [
     },
     {
         name: "BuildNutrientStation",
-        tooltip: "Cost: 5 iron, 1 floor space",
+        tooltip: "Cost: 5 iron, 1 floor space\nEffects: 1 food per second",
         function: craft,
         args: {
             iron: -5,
@@ -117,34 +137,79 @@ var actions = [
     },
     {
         name: "ExpandSpacecraft",
-        tooltip: "Cost: 20 iron",
+        tooltip: "Cost: 10 iron\nEffects: 1 floor space",
         function: craft,
         args: {
-            iron: -20,
-            availableFloorSpace: 1,
+            iron: -10,
+            floorSpace: 1,
         },
         duration: 10,
         enableOn: function() {return haveItems2(this) || myItems.crewMembers >= 1},
         usable: haveItems,
     },
-    // {
-    //     name: "BuildMatterEnergyConverter",
-    //     tooltip: "Cost: 10 iron, 20 silicon, 1 floor space",
-    //     function: craft,
-    //     args: {
-    //         iron: -10,
-    //         silicon: -20,
-    //         availableFloorSpace: -1,
-    //         matterEnergyConverters: 1,
-    //     },
-    //     duration: 4,
-    //     enableOn: function() {return availableFloorSpace >= 2},
-    //     usable: haveItems,
-    // },
+    {
+        name: "ManTheHelm",
+        tooltip: "Cost: 10 energy per thruster\n Effects: Navigate forward thrusters/floorSpace",
+        function: craft,
+        args: {
+            energy: -10,
+            customFunction: function() {
+                if (!myItems.distance) myItems.distance = 0;
+                if (typeof myItems.thrusters === "undefined") myItems.thrusters = 1;
+                myItems.distance += myItems.thrusters / myItems.floorSpace;
+            },
+        },
+        duration: 10,
+        enableOn: function() {return haveItems2(this) && myItems.thrusters >= 1},
+        usable: function() {return haveItems2(this) && myItems.thrusters >= 1},
+        maxWorkers: 1,
+    },
+    {
+        name: "BuildCargoContainer",
+        tooltip: "Cost: 10 iron\nEffects: +100 cargo storage",
+        function: craft,
+        args: {
+            iron: -10,
+            availableFloorSpace: -1,
+            cargoContainers: 1,
+        },
+        duration: 4,
+        enableOn: function() {return myItems.ironOre >= 2 || myItems.siliconOre >= 2},
+        usable: haveItems,
+    },
+    {
+        name: "BuildThruster",
+        tooltip: "Cost: 20 iron, 20 silicon\nEffects 1 thruster",
+        function: craft,
+        args: {
+            iron: -20,
+            silicon: -20,
+            customFunction: function() {
+                if (typeof myItems.thrusters === "undefined") myItems.thrusters = 1;
+                myItems.thrusters += 1;
+                actions[8].args.energy = -10 * myItems.thrusters;
+            },
+        },
+        duration: 10,
+        enableOn: function() {return myItems.ironOre >= 10},
+        usable: haveItems,
+    },
+    {
+        name: "ConvertMatterToEnergy",
+        tooltip: "Cost: 1 item of your choice\nEffects: 1 energy",
+        function: craft,
+        args: {
+            nothing: -1,
+            energy: 1,
+        },
+        duration: 2,
+        enableOn: function() {return myItems.siliconOre > 0 || myItems.ironOre > 0 || myItems.deadBodies > 0},
+        usable: haveItems,
+    },
 ];
 
-var inventoryDiv = $( "#inventory" );
-var actionsDiv = $( "#actions" );
+var inventoryDiv = $("#inventory");
+var actionsDiv = $("#actions");
 
 var state = null;
 var progress = 0;
@@ -162,11 +227,18 @@ function countAssignedWorkers()
 
 function updateCargoSpace()
 {
-    myItems.availableCargoSpace = 100
+    myItems.availableCargoSpace = 30 +
+        (myItems.cargoContainers?myItems.cargoContainers:0) * 100
         - (myItems.ironOre?myItems.ironOre:0)
         - (myItems.siliconOre?myItems.siliconOre:0)
         - (myItems.iron?myItems.iron:0)
         - (myItems.silicon?myItems.silicon:0);
+
+    myItems.availableFloorSpace =
+        (myItems.floorSpace?myItems.floorSpace:0)
+        - (myItems.solarPanels?myItems.solarPanels:0)
+        - (myItems.nutrientStations?myItems.nutrientStations:0)
+        - (myItems.cargoContainers?myItems.cargoContainers:0);
 }
 
 function updateInventory()
@@ -207,23 +279,52 @@ function updateActions()
             tooltipDiv.append(action.minusButton);
             tooltipDiv.append(action.workerCountSpan);
             actionsDiv.append(tooltipDiv);
+            if (action.name == "ConvertMatterToEnergy") {
+                convertDropdown = $("<select id='matter'></select>");
+                actionsDiv.append(convertDropdown);
+                convertDropdown.append($("<option value='nothing'>Select an item to destroy...</option>"));
+                // for (item in myItems) {
+                //     if (destroyableItems.includes(item)) {
+                //         convertDropdown.append($("<option value='" + item + "'>" + item + "</option>"));
+                //     }
+                // }
+                for (i in destroyableItems) {
+                    item = destroyableItems[i];
+                    convertDropdown.append($("<option value='" + item + "'>" + item + "</option>"));
+                }
+                convertDropdown.change(function() {
+                    actions[11].args = {
+                        energy: 1,
+                    };
+                    actions[11].args[convertDropdown.val()] = -1;
+                });
+            }
             actionsDiv.append($("<br />"));
             if (!action.assignedWorkers) action.assignedWorkers = 0;
             action.plusButton.click(function(e){
+                if (action2.maxWorkers) {
+                    let workersPlusPlayer = action2.assignedWorkers;
+                    if (state == action2) workersPlusPlayer += 1;
+                    if (workersPlusPlayer >= action2.maxWorkers) return;
+                }
                 if (myItems.crewMembers > countAssignedWorkers()) {
                     action2.assignedWorkers += 1;
                     action2.workerCountSpan.text(action2.assignedWorkers)
                 }
+                updateActions();
             });
             action.minusButton.click(function(e){
                 if (action2.assignedWorkers > 0) {
                     action2.assignedWorkers -= 1;
                     action2.workerCountSpan.text(action2.assignedWorkers)
                 }
+                updateActions();
             });
         }
         if (action.button) {
-            action.button.prop("disabled",!action.usable());
+            let disabled = !action.usable();
+            if (action.maxWorkers && action.assignedWorkers >= action.maxWorkers) disabled = true;
+            action.button.prop("disabled",disabled);
         }
     }
 }
@@ -240,7 +341,11 @@ function updateProgressBar()
 
 function search(itemsAtLocation)
 {
-    myItems.energy -= 4;
+    if (myItems.energy < 2) return;
+    updateCargoSpace();
+    if (myItems.availableCargoSpace < 1) return;
+
+    myItems.energy -= 2;
 
     let r = Math.random();
     let r2 = 0;
@@ -257,16 +362,22 @@ function craft(items)
 {
     // check if we have all the needed items
     for (item in items) {
-        let amount = items[item];
-        if (amount < 0 && !myItems[item] || myItems[item] < -amount) return;
+        if (item != "customFunction") {
+            let amount = items[item];
+            if (amount < 0 && !myItems[item] || myItems[item] < -amount) return;
+        }
     }
 
     // update amounts in inventory
     for (item in items) {
-        let amount = items[item];
-        if (!myItems[item]) myItems[item] = 0;
-        myItems[item] += amount;
+        if (item != "customFunction") {
+            let amount = items[item];
+            if (!myItems[item]) myItems[item] = 0;
+            myItems[item] += amount;
+        }
     }
+
+    if (items.customFunction) items.customFunction();
 }
 
 function killCrewMember()
@@ -280,6 +391,8 @@ function killCrewMember()
             return;
         }
     }
+    if (!myItems.deadBodies) myItems.deadBodies = 0;
+    myItems.deadBodies += 1;
 }
 
 function enableWorkerAssignment()
@@ -305,6 +418,7 @@ $(document).ready(function()
     {
         let needToUpdateScreen = false;
 
+        // advance my action
         if (state) {
             progress += 20 / state.duration;
             if (progress >= 100) {
@@ -315,6 +429,7 @@ $(document).ready(function()
             }
         }
 
+        // advance worker actions
         for (i in actions) {
             action = actions[i];
             if (action.assignedWorkers > 0) {
@@ -339,10 +454,10 @@ $(document).ready(function()
             }
             if (myItems.nutrientStations && myItems.energy >= myItems.nutrientStations) {
                 myItems.energy -= myItems.nutrientStations;
-                if (myItems.energy > myItems.availableEnergyStorage) myItems.energy = myItems.availableEnergyStorage;
                 if (!myItems.food) myItems.food = 0;
                 myItems.food += myItems.nutrientStations;
             }
+            if (myItems.energy > myItems.availableEnergyStorage) myItems.energy = myItems.availableEnergyStorage;
             if (myItems.crewMembers) {
                 if (!myItems.food) myItems.food = 0;
                 while (myItems.food < myItems.crewMembers) {
@@ -361,6 +476,6 @@ $(document).ready(function()
             updateActions();
         }
         updateProgressBar();
-    }, 200);
+    }, 20);
 
 });
